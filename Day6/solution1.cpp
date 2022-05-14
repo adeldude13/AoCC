54faf8c8b7d4294ef31a7f06e166ccb2f021a43d
adeldude13 <adeldude13@protonmail.com>
2022-05-14 20:02:22 +0300

    Finished Day6

---
+++ Day6/solution1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <boost/algorithm/string.hpp>
+#include <inttypes.h>
+#include <unordered_map>
+
+using namespace std;
+
+#define DAYS 80
+
+int main(int argc, char **argv) {
+	if(argc != 3) {
+		cout << argv[1] << " " << "filename " << endl;
+		return 1;
+	}
+	ifstream file(argv[1]);
+	string line;
+	unordered_map<uint64_t, uint64_t> umap;
+	for(int i=0; i<=8;i++) {
+		umap[i] = 0;
+	}
+	getline(file, line);
+	vector<string> temp;
+	vector<uint8_t> fishes;
+	boost::split(temp, line, boost::is_any_of(","));
+	for(int i=0; i<temp.size();i++) {
+		fishes.push_back(stoi(temp[i]));
+	}
+	for(int i=0; i<fishes.size();i++) {
+		umap[fishes[i]] += 1;
+	}
+	uint64_t count = fishes.size();
+	int j = 0;
+	while(j < DAYS) {
+		uint64_t tmp = umap[0];
+		umap[0] = 0;
+		for(int i=1;i<=8;i++) {
+				umap[i-1] += umap[i];
+				umap[i] = 0;
+		}
+		count += tmp;
+		umap[8] += tmp;
+		umap[6] += tmp;
+		j++;
+	}
+	
+	cout << count;	
+	return 0;
+}
